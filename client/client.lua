5d61908a46bd39d96e04e5e99aa08e61875e7de1
Xakra <zarkcat@gmail.com>
2024-12-28 02:59:54 +0100

    Preventing duplicate sound, some code improvements

--- client/client.lua
+++ client/client.lua
@@ -1,117 +1,108 @@
-function loadAnimDict(dict)
-	while (not HasAnimDictLoaded(dict)) do
-		RequestAnimDict(dict)
-		Wait(5)
-	end
-end
+local Particles = {}
+local Objects = {}
 
 -- BIG FIREWORKS
 RegisterNetEvent('xakra_fireworks:big_fireworks')
 AddEventHandler('xakra_fireworks:big_fireworks', function()
-    TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 7000, true, false, false, false)
+    TaskStartScenarioInPlace(PlayerPedId(), joaat('WORLD_HUMAN_CROUCH_INSPECT'), 7000, true, false, false, false)
     Wait(7000)
 
-    local big_fireworks_object = CreateObject("p_wickerbox01x", GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.0, 0.0), true, true, false)
+    local big_fireworks_object = CreateObject('p_wickerbox01x', GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.0, 0.0), true, true, false)
+    table.insert(Objects, big_fireworks_object)
     PlaceObjectOnGroundProperly(big_fireworks_object)
     SetEntityHeading(big_fireworks_object, GetEntityHeading(PlayerPedId()))
 
     Wait(5000)
-    TriggerServerEvent("xakra_fireworks:players", GetEntityCoords(big_fireworks_object), "big_fireworks")
+    local coords = GetEntityCoords(big_fireworks_object)
+    TriggerServerEvent('xakra_fireworks:players', coords, 'big_fireworks')
 
-    Wait(Config.big_fireworks_time * 1000 + 10000)
+    local stop = false
+    SetTimeout(Config.small_fireworks_time * 1000, function()
+        stop = true
+    end)
+
+    repeat
+        AddExplosionWithUserVfx(coords, 32, 0x2FDA2B4A, 0.0, Config.EnableSound, false, false, 0.0)
+        Wait(100)
+        AddExplosionWithUserVfx(coords.x, coords.y, coords.z + 60, 25, 0x2FDA2B4A, 0.0, Config.EnableSound, true, false, 0.0)
+        Wait(math.random(100,800))
+    until stop
+
+    Wait(500)
+    AddExplosionWithUserVfx(coords, 32, 0x2FDA2B4A, 0.0, Config.EnableSound, false, false, 0.0) 
+    Wait(100)
+    AddExplosionWithUserVfx(coords.x, coords.y, coords.z + 60, 25, 0x2FDA2B4A, 0.0, Config.EnableSound, true, false, 0.0) 
+
+    Wait(10000)
     DeleteObject(big_fireworks_object)
 end)
 
 RegisterNetEvent('xakra_fireworks:big_fireworks_players')
 AddEventHandler('xakra_fireworks:big_fireworks_players', function(coords)
-    RequestNamedPtfxAsset(GetHashKey('scr_ind1'))
-    while not HasNamedPtfxAssetLoaded(GetHashKey('scr_ind1')) do
-        Wait(10)
-    end
-    UseParticleFxAsset('scr_ind1')
-    local fireworks = StartParticleFxLoopedAtCoord('scr_ind1_firework', coords, 0.0, 0.0, 0.0, 1.0, false, false, false, true) 
+    LoadPtfxAsset('scr_ind1')
+    local fireworks = StartParticleFxLoopedAtCoord('scr_ind1_firework', coords, 0.0, 0.0, 0.0, 1.0, false, false, false, true)
+    table.insert(Particles, fireworks)
 
-    local stop
-    Citizen.CreateThread(function()
-        Wait(Config.big_fireworks_time * 1000)
-        stop = true
-    end)
-
-    while not stop do
-        Citizen.InvokeNative(0x53BA259F3A67A99E, coords, 32, 0xF36AD9AC, 0.0, Config.EnableSound, false, true, 0.0)   -- AddExplosionWithUserVfx
-        Wait(100)
-        Citizen.InvokeNative(0x53BA259F3A67A99E, coords.x, coords.y, coords.z + 60, 25, 0xF36AD9AC, 0.0, Config.EnableSound, true, true, 0.0)   -- AddExplosionWithUserVfx
-
-        Wait(math.random(100,800))
-    end
+    Wait(Config.small_fireworks_time * 1000)
 
     StopParticleFxLooped(fireworks, true)
-
-    Wait(500)
-    Citizen.InvokeNative(0x53BA259F3A67A99E, coords, 32, 0xF36AD9AC, 0.0, Config.EnableSound, false, true, 0.0)   -- AddExplosionWithUserVfx
-    Wait(100)
-    Citizen.InvokeNative(0x53BA259F3A67A99E, coords.x, coords.y, coords.z + 60, 25, 0xF36AD9AC, 0.0, Config.EnableSound, true, true, 0.0)   -- AddExplosionWithUserVfx
 end)
 
 -- SMALL FIREWORKS
 RegisterNetEvent('xakra_fireworks:small_fireworks')
 AddEventHandler('xakra_fireworks:small_fireworks', function()
-    TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 7000, true, false, false, false)
+    TaskStartScenarioInPlace(PlayerPedId(), joaat('WORLD_HUMAN_CROUCH_INSPECT'), 7000, true, false, false, false)
     Wait(7000)
 
-    local small_fireworks_object = CreateObject("p_wickerbox01x", GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.0, 0.0), true, true, false)
+    local small_fireworks_object = CreateObject('p_wickerbox01x', GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 1.0, 0.0), true, true, false)
+    table.insert(Objects, small_fireworks_object)
     PlaceObjectOnGroundProperly(small_fireworks_object)
     SetEntityHeading(small_fireworks_object, GetEntityHeading(PlayerPedId()))
 
     Wait(5000)
-    TriggerServerEvent("xakra_fireworks:players", GetEntityCoords(small_fireworks_object), "small_fireworks")
+    local coords = GetEntityCoords(small_fireworks_object)
+    TriggerServerEvent('xakra_fireworks:players', coords, 'small_fireworks')
 
-    Wait(Config.small_fireworks_time * 1000 + 10000)
+    local stop = false
+    SetTimeout(Config.small_fireworks_time * 1000, function()
+        stop = true
+    end)
+
+    repeat
+        AddExplosionWithUserVfx(coords, 32, 0x2FDA2B4A, 0.0, Config.EnableSound, false, false, 0.0)  
+        Wait(100)
+        AddExplosionWithUserVfx(coords.x, coords.y, coords.z + 10, 12, 0x2FDA2B4A, 0.0, Config.EnableSound, true, false, 0.0)
+        Wait(math.random(100,800))
+    until stop
+
+    Wait(500)
+    AddExplosionWithUserVfx(coords, 32, 0x2FDA2B4A, 0.0, Config.EnableSound, false, false, 0.0)  
+    Wait(100)
+    AddExplosionWithUserVfx(coords.x, coords.y, coords.z + 10, 12, 0x2FDA2B4A, 0.0, Config.EnableSound, true, false, 0.0)
+
+    Wait(10000)
     DeleteObject(small_fireworks_object)
 end)
 
 RegisterNetEvent('xakra_fireworks:small_fireworks_players')
 AddEventHandler('xakra_fireworks:small_fireworks_players', function(coords)
-    RequestNamedPtfxAsset(GetHashKey('anm_ind'))
-    while not HasNamedPtfxAssetLoaded(GetHashKey('anm_ind')) do
-        Wait(10)
-    end
-    UseParticleFxAsset('anm_ind')
-    local fireworks = StartParticleFxLoopedAtCoord('ent_anim_ind_firework', coords, 0.0, 0.0, 0.0, 0.20, false, false, false, true) 
+    LoadPtfxAsset('anm_ind')
+    local fireworks = StartParticleFxLoopedAtCoord('ent_anim_ind_firework', coords, 0.0, 0.0, 0.0, 0.20, false, false, false, true)
+    table.insert(Particles, fireworks)
 
-    local stop
-    Citizen.CreateThread(function()
-        Wait(Config.small_fireworks_time * 1000)
-        stop = true
-    end)
-
-    while not stop do
-        Citizen.InvokeNative(0x53BA259F3A67A99E, coords, 32, 0xF36AD9AC, 0.0, Config.EnableSound, false, true, 0.0)    -- AddExplosionWithUserVfx
-        Wait(100)
-        Citizen.InvokeNative(0x53BA259F3A67A99E, coords.x, coords.y, coords.z + 10, 12, 0xF36AD9AC, 0.0, Config.EnableSound, true, true, 0.0)  -- AddExplosionWithUserVfxn
-        Wait(math.random(100,800))
-    end
+    Wait(Config.small_fireworks_time * 1000)
 
     StopParticleFxLooped(fireworks, true)
-
-    Wait(500)
-    Citizen.InvokeNative(0x53BA259F3A67A99E, coords, 32, 0xF36AD9AC, 0.0, Config.EnableSound, false, true, 0.0)    -- AddExplosionWithUserVfx
-    Wait(100)
-    Citizen.InvokeNative(0x53BA259F3A67A99E, coords.x, coords.y, coords.z + 10, 12, 0xF36AD9AC, 0.0, Config.EnableSound, true, true, 0.0)  -- AddExplosionWithUserVfx
 end)
 
 -- BIG FIRECRACKER
 RegisterNetEvent('xakra_fireworks:big_firecracker')
 AddEventHandler('xakra_fireworks:big_firecracker', function()
-    local animDict = "amb_camp@world_camp_jack_throws_rocks_casual@male_a@wip_base"
-    local animName = "base"
-    local duration = 6500
-    local flags = 2
-    loadAnimDict(animDict)
-    TaskPlayAnim(PlayerPedId(), animDict, animName, 8.0, 3.0, duration, flags, 0, 0, 0, 0)
+    Anim('amb_camp@world_camp_jack_throws_rocks_casual@male_a@wip_base', 'base', 6500, 2)
     
-    local big_firecracker_object = CreateObject(GetHashKey("w_throw_dynamite01"), GetEntityCoords(PlayerPedId()), true, true, true)
-    local righthand = GetEntityBoneIndexByName(PlayerPedId(), "IK_R_Hand")
+    local big_firecracker_object = CreateObject(joaat('w_throw_dynamite01'), GetEntityCoords(PlayerPedId()), true, true, true)
+    table.insert(Objects, big_firecracker_object)
+    local righthand = GetEntityBoneIndexByName(PlayerPedId(), 'IK_R_Hand')
     AttachEntityToEntity(big_firecracker_object, PlayerPedId(), righthand, 0.06, 0.0, -0.05, -90.0, 0.0, -45.0, true, true, false, true, 1, true)
 
     Wait(4500)
@@ -120,41 +111,32 @@ AddEventHandler('xakra_fireworks:big_firecracker', function()
 
     local coords = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 15.0, 0.0)
     local _, z = GetGroundZAndNormalFor_3dCoord(coords.x, coords.y, coords.z + 100)
-    TriggerServerEvent("xakra_fireworks:players", vector3(coords.x, coords.y, z), "big_firecracker")
-end)
 
-RegisterNetEvent('xakra_fireworks:big_firecracker_players')
-AddEventHandler('xakra_fireworks:big_firecracker_players', function(coords)
-    Citizen.InvokeNative(0x53BA259F3A67A99E, coords, 31, 0xC22E127D, 0.0, true, false, true, 0.0)   -- AddExplosionWithUserVfx
+    AddExplosionWithUserVfx(coords.x, coords.y, z, 31, 0x2FDA2B4A, 0.0, true, false, false, 0.0)
+
+    LoadPtfxAsset('core')
+    StartNetworkedParticleFxNonLoopedAtCoord('exp_grd_smoke_post_volatile', coords.x, coords.y, z, 0.0, 0.0, 0.0, 1.0, false, false, false)
 end)
 
 -- SMALL FIRECRACKER
 RegisterNetEvent('xakra_fireworks:small_firecracker')
 AddEventHandler('xakra_fireworks:small_firecracker', function()
-    local animDict = "amb_camp@world_camp_jack_throws_rocks_casual@male_a@wip_base"
-    local animName = "base"
-    local duration = 6500
-    local flags = 2
-    loadAnimDict(animDict) -- Función para cargar animación.
-    TaskPlayAnim(PlayerPedId(), animDict, animName, 8.0, 3.0, duration, flags, 0, 0, 0, 0)
+    Anim('amb_camp@world_camp_jack_throws_rocks_casual@male_a@wip_base', 'base', 6500, 2)
 
-    local small_firecracker_object = CreateObject(GetHashKey("mp001_p_mp_dynamite01x_noexp"), GetEntityCoords(PlayerPedId()), true, true, true)
-    local righthand = GetEntityBoneIndexByName(PlayerPedId(), "IK_R_Hand")
+    local small_firecracker_object = CreateObject(joaat('mp001_p_mp_dynamite01x_noexp'), GetEntityCoords(PlayerPedId()), true, true, true)
+    table.insert(Objects, small_firecracker_object)
+    local righthand = GetEntityBoneIndexByName(PlayerPedId(), 'IK_R_Hand')
     AttachEntityToEntity(small_firecracker_object, PlayerPedId(), righthand, 0.09, 0.0, -0.04, -90.0, 0.0, -43.0, true, true, false, true, 1, true)
 
-    Wait(4500) -- Espera para animaciones.
+    Wait(4500)
     DeleteObject(small_firecracker_object)
-    Wait(1500) -- Espera para animaciones.
+    Wait(1500)
 
     local coords = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 15.0, 0.0)
     local _, z = GetGroundZAndNormalFor_3dCoord(coords.x, coords.y, coords.z + 100)
-    TriggerServerEvent("xakra_fireworks:players", vector3(coords.x, coords.y, z), "small_firecracker")
-end)
-
-RegisterNetEvent('xakra_fireworks:small_firecracker_players')
-AddEventHandler('xakra_fireworks:small_firecracker_players', function(coords)
+    
     for i = 1, 3 do
-        Citizen.InvokeNative(0x53BA259F3A67A99E, coords, 12, 0x2FDA2B4A, 0.0, true, false, true, 0.0)  -- AddExplosionWithUserVfx
+        AddExplosionWithUserVfx(coords.x, coords.y, z, 12, 0x2FDA2B4A, 0.0, true, false, true, 0.0)
         Wait(300)
     end
 end)
@@ -164,14 +146,15 @@ RegisterNetEvent('xakra_fireworks:smoke_campfire')
 AddEventHandler('xakra_fireworks:smoke_campfire', function()
     local coords = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, 0.0)
 
-    TaskStartScenarioInPlace(PlayerPedId(), GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 12000, true, false, false, false)
+    TaskStartScenarioInPlace(PlayerPedId(), joaat('WORLD_HUMAN_CROUCH_INSPECT'), 12000, true, false, false, false)
     Wait(12000)
 
-    local smoke_campfire_object = CreateObject("p_campfire01x", coords, true, true, false)
+    local smoke_campfire_object = CreateObject('p_campfire01x', coords, true, true, false)
+    table.insert(Objects, smoke_campfire_object)
     PlaceObjectOnGroundProperly(smoke_campfire_object)
     SetEntityHeading(smoke_campfire_object, GetEntityHeading(PlayerPedId()))
 
-    TriggerServerEvent("xakra_fireworks:players", GetEntityCoords(smoke_campfire_object), "smoke_campfire")
+    TriggerServerEvent('xakra_fireworks:players', GetEntityCoords(smoke_campfire_object), 'smoke_campfire')
 
     Wait(Config.smoke_campfire_time * 1000 + 10000)
     DeleteObject(smoke_campfire_object)
@@ -179,14 +162,60 @@ end)
 
 RegisterNetEvent('xakra_fireworks:smoke_campfire_players')
 AddEventHandler('xakra_fireworks:smoke_campfire_players', function(coords)
-    RequestNamedPtfxAsset(GetHashKey('SCR_ADV_SOK'))
-    while not HasNamedPtfxAssetLoaded(GetHashKey('SCR_ADV_SOK')) do
-        Wait(10)
-    end
-
-    UseParticleFxAsset("SCR_ADV_SOK")
-    local smoke = StartParticleFxLoopedAtCoord("scr_adv_sok_torchsmoke", coords.x, coords.y, coords.z - 6.22, 0.0,0.0,0.0, 8.0, false, false, false, true)
+    LoadPtfxAsset('scr_native1')
+    local smoke = StartParticleFxLoopedAtCoord('scr_native1_campfire_smoke', coords.x, coords.y, coords.z + 3.5, 0.0, 0.0, 0.0, 3.0, false, false, false, true)
+    SetParticleFxLoopedAlpha(smoke, 255.0)
+    table.insert(Particles, smoke)
     
     Wait(Config.smoke_campfire_time * 1000)
     StopParticleFxLooped(smoke, true)
+end)
+
+-- LOAD PTFXASSET
+function LoadPtfxAsset(fxName)
+    if not HasNamedPtfxAssetLoaded(joaat(fxName)) then
+        RequestNamedPtfxAsset(joaat(fxName))
+
+        while not HasNamedPtfxAssetLoaded(joaat(fxName)) do
+            Wait(0)
+        end
+    end
+
+    UseParticleFxAsset(fxName)
+end
+
+-- ANIM
+function Anim(animDict, animName, duration, flag, introtiming, exittiming)
+    if not HasAnimDictLoaded(animDict) then
+        RequestAnimDict(animDict)
+
+        local t = 500
+
+        while not HasAnimDictLoaded(animDict) and t > 0 do
+            t = t - 1
+            Wait(0)
+        end
+    end
+
+    TaskPlayAnim(PlayerPedId(), animDict, animName, tonumber(introtiming) or 1.0, tonumber(exittiming) or 1.0, duration or -1, flag or 1, 1, false, false, false, 0, true)
+    RemoveAnimDict(animDict)
+end
+
+-- RESOURCE STOP
+AddEventHandler('onResourceStop', function (resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+
+    for _, v in pairs(Particles) do
+        if DoesParticleFxLoopedExist(v) then
+            StopParticleFxLooped(v, true)
+        end
+    end
+
+    for _, v in pairs(Objects) do
+        if DoesEntityExist(v) then
+            DeleteObject(v)
+        end
+    end
 end)
--- server/server.lua
+++ server/server.lua
@@ -1,57 +1,45 @@
 
-local VorpCore = {}
+local VorpCore = exports.vorp_core:GetCore()
 
-TriggerEvent("getCore",function(core)
-    VorpCore = core
-end)
+RegisterServerEvent('xakra_fireworks:players')
+AddEventHandler('xakra_fireworks:players', function(coords, firework)
+	if firework == 'big_fireworks' then
+		TriggerClientEvent('xakra_fireworks:big_fireworks_players', -1, coords)
 
-VorpInv = exports.vorp_inventory:vorp_inventoryApi()
+	elseif firework == 'small_fireworks' then
+		TriggerClientEvent('xakra_fireworks:small_fireworks_players', -1, coords)
 
-RegisterServerEvent("xakra_fireworks:players")
-AddEventHandler("xakra_fireworks:players", function(coords, firework)
-	if firework == "big_fireworks" then
-		TriggerClientEvent("xakra_fireworks:big_fireworks_players", -1, coords)
-
-	elseif firework == "small_fireworks" then
-		TriggerClientEvent("xakra_fireworks:small_fireworks_players", -1, coords)
-
-	elseif firework == "big_firecracker" then
-		TriggerClientEvent("xakra_fireworks:big_firecracker_players", -1, coords)
-
-	elseif firework == "small_firecracker" then
-		TriggerClientEvent("xakra_fireworks:small_firecracker_players", -1, coords)
-
-	elseif firework == "smoke_campfire" then
-		TriggerClientEvent("xakra_fireworks:smoke_campfire_players", -1, coords)
+	elseif firework == 'smoke_campfire' then
+		TriggerClientEvent('xakra_fireworks:smoke_campfire_players', -1, coords)
 	end
 end)
 
-VorpInv.RegisterUsableItem("big_fireworks", function(data)
-	VorpInv.subItem(data.source, "big_fireworks", 1)  
-	TriggerClientEvent("xakra_fireworks:big_fireworks", data.source)
-	VorpInv.CloseInv(data.source)
+exports.vorp_inventory:registerUsableItem('big_fireworks', function(data)
+	exports.vorp_inventory:subItem(data.source, 'big_fireworks', 1)  
+	TriggerClientEvent('xakra_fireworks:big_fireworks', data.source)
+	exports.vorp_inventory:closeInventory(data.source)
 end)
 
-VorpInv.RegisterUsableItem("small_fireworks", function(data) 
-	VorpInv.subItem(data.source, "small_fireworks", 1) 
-	TriggerClientEvent("xakra_fireworks:small_fireworks", data.source)
-	VorpInv.CloseInv(data.source)
+exports.vorp_inventory:registerUsableItem('small_fireworks', function(data) 
+	exports.vorp_inventory:subItem(data.source, 'small_fireworks', 1) 
+	TriggerClientEvent('xakra_fireworks:small_fireworks', data.source)
+	exports.vorp_inventory:closeInventory(data.source)
 end)
 
-VorpInv.RegisterUsableItem("big_firecracker", function(data)
-	VorpInv.subItem(data.source, "big_firecracker", 1)
-	TriggerClientEvent("xakra_fireworks:big_firecracker", data.source)
-	VorpInv.CloseInv(data.source)
+exports.vorp_inventory:registerUsableItem('big_firecracker', function(data)
+	exports.vorp_inventory:subItem(data.source, 'big_firecracker', 1)
+	TriggerClientEvent('xakra_fireworks:big_firecracker', data.source)
+	exports.vorp_inventory:closeInventory(data.source)
 end)
 
-VorpInv.RegisterUsableItem("small_firecracker", function(data)
-	VorpInv.subItem(data.source, "small_firecracker", 1)
-	TriggerClientEvent("xakra_fireworks:small_firecracker", data.source)
-	VorpInv.CloseInv(data.source)
+exports.vorp_inventory:registerUsableItem('small_firecracker', function(data)
+	exports.vorp_inventory:subItem(data.source, 'small_firecracker', 1)
+	TriggerClientEvent('xakra_fireworks:small_firecracker', data.source)
+	exports.vorp_inventory:closeInventory(data.source)
 end)
 
-VorpInv.RegisterUsableItem("smoke_campfire", function(data)
-	VorpInv.subItem(data.source, "smoke_campfire", 1)
-	TriggerClientEvent("xakra_fireworks:smoke_campfire", data.source)
-	VorpInv.CloseInv(data.source)
+exports.vorp_inventory:registerUsableItem('smoke_campfire', function(data)
+	exports.vorp_inventory:subItem(data.source, 'smoke_campfire', 1)
+	TriggerClientEvent('xakra_fireworks:smoke_campfire', data.source)
+	exports.vorp_inventory:closeInventory(data.source)
 end)
